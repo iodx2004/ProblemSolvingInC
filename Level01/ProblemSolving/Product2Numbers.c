3ea47588f2488a896c161f9f8cdbd6db217dee25
iodx2004 <iodx2004@github.com>
2025-07-11 15:01:23 +0000

    Level01

---
+++ Level01/ProblemSolving/Product2Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int Product2Numbers(int a, int b){
+
+  return a * b;
+
+}
+
+int main(){
+
+  int a = 15;
+  int b = 25;
+
+  printf("The Result is : %d\n", Product2Numbers(a, b));
+
+}
