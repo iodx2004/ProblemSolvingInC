f9cb1a1e7c01695bafe097198451b083820cad2e
iodx2004 <iodx2004@github.com>
2025-07-21 14:36:52 +0000

    level01

--- Level01/ProblemSolving/calculateSum.c
+++ Level01/ProblemSolving/calculateSum.c
@@ -5,10 +5,18 @@ float SumNumberMath(void){
   float S = 0;
   int i;
 
-  while(){
+  i = 1;
+  while(i <= 50){
 
-    
-
+    S += (float)1 / i;
+    i++;
   }
+
+  printf("The Value of S is : %.2f\n", S);
+}
+
+int main(){
+
+  SumNumberMath();
 
 }
